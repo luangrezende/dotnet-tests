31333366f0d8c2bdbd49ffaab49fd638cadb5472
Luan Gomes <luangrezende@gmail.com>
2022-04-03 17:11:24 -0400

    implementado algoritomo megaSena

--- Algorithms/MegaSena.cs
+++ Algorithms/MegaSena.cs
@@ -1,21 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TestApp.Algorithms
+﻿namespace TestApp.Algorithms
 {
     public class MegaSena
     {
-        public Array[] GetWinner()
-        {
+        public List<List<int>> NumerosJogados { get; set; }
 
+        public MegaSena() {
+            NumerosJogados = new List<List<int>>();
+            GerarNumerosJogados();
         }
 
-        private bool VerifyIfNumbersExists()
+        public void GetWinner()
         {
+            var encontrandoNumero = true;
 
+            while (encontrandoNumero)
+            {
+                var numeroSorteado = GerarNumeroSorteado();
+
+                //teste apenas
+                //NumerosJogados.Add(numeroSorteado);
+
+                var temGanhador = VerifyIfAllNumbersExists(numeroSorteado);
+
+                if (temGanhador)
+                    Console.WriteLine("NUMERO EXISTENTE NA LISTA - RODANDO NOVAMENTE...");
+                else
+                {
+                    Console.WriteLine($"NÃO TEM GANHADOR NA LISTA - NUMERO SORTEADO: {string.Join(", ", numeroSorteado)}");
+                    encontrandoNumero = false;
+                }
+            }
+        }
+
+        private bool VerifyIfAllNumbersExists(List<int> numeroSorteado)
+        {
+            var limitePontos = 6;
+            var pontos = 0;
+
+            foreach (var numbersPlayed in NumerosJogados)
+            {
+                pontos = 0;
+                foreach (var number in numeroSorteado)
+                {
+                    var contais = numbersPlayed.Contains(number);
+
+                    if (contais)
+                        pontos++;
+                }
+            }
+
+            return pontos >= limitePontos;
+        }
+
+        public void GerarNumerosJogados()
+        {
+            var limit = 150;
+            var quantidadeNumerosGerados = 0;
+
+            while (limit > quantidadeNumerosGerados)
+            {
+                NumerosJogados.Add(GerarNumeroSorteado());
+                quantidadeNumerosGerados++;
+            }
+        }
+
+        public List<int> GerarNumeroSorteado()
+        {
+            var numeroSorteado = new List<int>();
+            for (int i = 0; i < 6; i++)
+            {
+                var random = new Random();
+                var numeroGerado = random.Next(1, 99);
+
+                numeroSorteado.Add(numeroGerado);
+            }
+
+            numeroSorteado.Sort();
+            return numeroSorteado;
         }
     }
 }
