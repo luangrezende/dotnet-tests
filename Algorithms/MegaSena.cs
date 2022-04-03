c43b10a1dfc3eca66a74ba6047b3b574f8f72ce6
Luan Gomes <luangrezende@gmail.com>
2022-04-03 15:46:13 -0400

    upload project

---
+++ Algorithms/MegaSena.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.Algorithms
+{
+    public class MegaSena
+    {
+        public Array[] GetWinner()
+        {
+
+        }
+
+        private bool VerifyIfNumbersExists()
+        {
+
+        }
+    }
+}
